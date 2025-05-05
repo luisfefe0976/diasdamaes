078bd32291e5cb05a0c099d21453f9cd167aac5e
luisfefe <00001093682644SP@al.educacao.sp.gov.br>
2025-05-05 13:11:11 -0300

    Add files via upload

---
+++ mamãe.py
@@ -0,0 +1,19 @@
+import time
+menssagem = [
+    "feliz dia da mães",
+    "❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️",
+    "Mãe,voce merece ser feliz todos os dias e hoje È a minha vez de fazer de tudo para voce esbanjar alegria",
+    "seu amor por mim È mais que tudo nesse mundo",
+    "❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️",
+    "mãe, vc não È so a minha melhor amiga È minha maior espiracão nesse mundo",
+]
+
+
+for linha in menssagem:
+    print(linha)
+    time.sleep(1.5) #espere 1.5 segundos antes de mostrar a próxima linha
+
+
+
+
+
